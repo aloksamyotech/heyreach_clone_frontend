d8d09f7d01afd4126ec3656e6fa48d737ef7263e
Deepak <deepak@samyotech.com>
2024-12-09 12:08:58 +0530

    Initial Start

---
+++ src/menu-items/dashboard.js
@@ -0,0 +1,56 @@
+// assets
+import {
+  IconHome,
+  IconCalendarEvent,
+  IconMail,
+  IconFileUpload,
+  IconFileInvoice,
+  IconPhoneCall,
+  IconAntennaBars5,
+  IconChecklist,
+  IconNotebook,
+  IconPhoneCheck,
+  IconUsers
+} from '@tabler/icons';
+
+// constant
+const icons = {
+  IconHome,
+  IconCalendarEvent,
+  IconMail,
+  IconFileUpload,
+  IconFileInvoice,
+  IconPhoneCall,
+  IconAntennaBars5,
+  IconChecklist,
+  IconNotebook,
+  IconPhoneCheck,
+  IconUsers
+};
+
+// ==============================|| DASHBOARD MENU ITEMS ||============================== //
+
+const dashboard = {
+  title: 'Dashboard-Menu',
+  type: 'group',
+  children: [
+    {
+      id: 'default',
+      title: 'Dashboard',
+      type: 'item',
+      url: '/dashboard/default',
+      icon: icons.IconHome,
+      breadcrumbs: false
+    },
+    {
+      id: '01',
+      title: 'Lead Management',
+      type: 'item',
+      url: '/dashboard/lead',
+      icon: icons.IconAntennaBars5,
+      breadcrumbs: false
+    }
+  ]
+};
+
+export default dashboard;
---
+++ src/routes/MainRoutes.js
@@ -0,0 +1,42 @@
+import { lazy } from 'react';
+
+// project imports
+import MainLayout from 'layout/MainLayout';
+import Loadable from 'ui-component/Loadable';
+
+// dashboard routing
+const DashboardDefault = Loadable(lazy(() => import('views/dashboard/Default')));
+const LeadManagement = Loadable(lazy(() => import('views/Lead')));
+
+// ==============================|| MAIN ROUTING ||============================== //
+
+const MainRoutes = {
+  path: '/',
+  element: <MainLayout />,
+  children: [
+    {
+      path: '/',
+      element: <DashboardDefault />
+    },
+    {
+      path: 'dashboard',
+      children: [
+        {
+          path: 'default',
+          element: <DashboardDefault />
+        }
+      ]
+    },
+    {
+      path: 'dashboard',
+      children: [
+        {
+          path: 'lead',
+          element: <LeadManagement />
+        }
+      ]
+    }
+  ]
+};
+
+export default MainRoutes;
