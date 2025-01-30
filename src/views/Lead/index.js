5526ae82efc743f3c0b05b25ab12090c908398a7
Dipesh Dabi <dipesh@samyotech.com>
2025-01-30 19:21:46 +0530

    real time data implementation

--- src/views/Lead/index.js
+++ src/views/Lead/index.js
@@ -1,21 +1,50 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react-hooks/exhaustive-deps */
 import { useState } from 'react';
-// @mui
-import { Stack, Button, Container, Typography, Box, Card } from '@mui/material';
+import {
+  Stack,
+  Button,
+  Container,
+  Typography,
+  Box,
+  Card,
+  TextField,
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Grid,
+  FormLabel
+} from '@mui/material';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
-
-import Iconify from '../../ui-component/iconify';
 import TableStyle from '../../ui-component/TableStyle';
-import AddLead from './AddLead.js';
+// import AddLead from './AddLead.js';
 import { useEffect } from 'react';
-import { fetchData } from 'api';
+import { fetchData, postData } from 'api';
+import { toast } from 'react-toastify';
+import CircularProgress from '@mui/material/CircularProgress';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { useForm } from 'react-hook-form';
 
 // ----------------------------------------------------------------------
 
 const Lead = () => {
-  const [openAdd, setOpenAdd] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    formState: { errors }
+  } = useForm();
+  const [isLoading, setLoader] = useState(false);
   const [leadData, setLeadData] = useState([]);
+  const [modal, setModal] = useState(false);
+  const handleOpen = () => {
+    setModal(true);
+  };
+  const handleClose = () => {
+    setModal(false);
+    reset();
+  };
+
   const columns = [
     {
       field: 'name',
@@ -42,42 +71,118 @@ const Lead = () => {
       headerName: 'Location',
       flex: 1
     }
-    // {
-    //   field: 'action',
-    //   headerName: 'Action',
-    //   flex: 1,
-    //   renderCell: (params) => {
-    //     return <Button variant="contained">Update Detail</Button>;
-    //   }
-    // }
   ];
 
-  const handleOpenAdd = () => setOpenAdd(true);
-  const handleCloseAdd = () => setOpenAdd(false);
-
-  const getData = async () => {
+  const getLead = async () => {
     const response = await fetchData('linkedin/getLeadData');
-    console.log('response : ', response);
-
     setLeadData(response?.data);
   };
 
+  const onSubmit = async (data) => {
+    try {
+      setLoader(true);
+      const response = await postData('linkedin/filterData', data);
+      console.log('response : ', response);
+      reset();
+      if (response.status === 201) {
+        toast.success('Lead Generated');
+      } else {
+        console.error('Failed to connect account');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      toast.error('Something Went Wrong');
+    } finally {
+      setLoader(false);
+    }
+  };
+
   useEffect(() => {
-    getData();
+    getLead();
+    const interval = setInterval(() => {
+      getLead();
+    }, 30000);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <>
+      {isLoading && (
+        <Box
+          sx={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(255, 255, 255, 0.1)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            zIndex: 9999
+          }}
+        >
+          <CircularProgress />
+          <Typography>Fetching Leads Data It Require 5-10 min</Typography>
+        </Box>
+      )}
+      <Dialog open={modal} onClose={handleClose} fullWidth aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+        {/* <DialogTitle id="alert-dialog-title">{'Enter Filter URL from LinkedIn'}</DialogTitle> */}
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <FormLabel>Enter Filter URL</FormLabel>
+                  <TextField
+                    {...register('url')}
+                    type="text"
+                    variant="outlined"
+                    fullWidth
+                    sx={{ marginTop: '8px' }}
+                    error={!!errors.url}
+                    helperText={errors.url?.message}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    fullWidth
+                    disabled={watch('url') ? false : true}
+                    sx={{
+                      height: '50px',
+                      borderRadius: '8px',
+                      backgroundColor: '#0A66C2',
+                      '&:hover': {
+                        backgroundColor: '#004182'
+                      }
+                    }}
+                    startIcon={<LinkedInIcon />}
+                  >
+                    Apply Filter
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
       {/* <AddLead open={openAdd} handleClose={handleCloseAdd} /> */}
       <Container>
-        {/* <Stack direction="row" alignItems="center" mb={5} justifyContent={'space-between'}>
+        <Stack direction="row" alignItems="center" mb={5} justifyContent={'space-between'}>
           <Typography variant="h4">Lead-Management</Typography>
           <Stack direction="row" alignItems="center" justifyContent={'flex-end'} spacing={2}>
-            <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />} onClick={handleOpenAdd}>
-              New Lead
+            <Button variant="contained" startIcon={<LinkedInIcon />} onClick={handleOpen}>
+              Generate Lead
             </Button>
           </Stack>
-        </Stack> */}
+        </Stack>
         <TableStyle>
           <Box width="100%">
             <Card style={{ height: '600px', paddingTop: '15px' }}>
--- src/views/LinkedAccounts/index.js
+++ src/views/LinkedAccounts/index.js
@@ -1,19 +1,4 @@
-import {
-  Stack,
-  Button,
-  Container,
-  Typography,
-  Box,
-  Card,
-  TextField,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
-  Grid,
-  FormLabel
-} from '@mui/material';
+import { Stack, Button, Container, Typography, Box, Card } from '@mui/material';
 import TableStyle from '../../ui-component/TableStyle';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import React from 'react';
@@ -21,29 +6,10 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import ConnectAccount from './ConnectAccount';
-import { fetchData, postData } from 'api';
-import { useForm } from 'react-hook-form';
-import { toast } from 'react-toastify';
-import CircularProgress from '@mui/material/CircularProgress';
+import { fetchData } from 'api';
 
 const LinkedAccounts = () => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    watch,
-    formState: { errors }
-  } = useForm();
-  const [isLoading, setLoader] = useState(false);
   const [openDrawer, setOpenDrawer] = useState(false);
-  const [modal, setModal] = React.useState(false);
-  const handleOpen = () => {
-    setModal(true);
-  };
-  const handleClose = () => {
-    setModal(false);
-    reset();
-  };
   const [linkedinData, setAccount] = useState([]);
   const toggleDrawer = (openDrawer) => {
     setOpenDrawer(openDrawer);
@@ -52,26 +18,6 @@ const LinkedAccounts = () => {
   const getData = async () => {
     const response = await fetchData('linkedin/get_linkedIn_account');
     setAccount(response?.data);
-  };
-
-  const onSubmit = async (data) => {
-    try {
-      setLoader(true);
-      const response = await postData('linkedin/filterData', data);
-      console.log('response : ', response);
-      reset();
-      if (response.ok) {
-        const result = await response.json();
-        console.log('Success:', result);
-      } else {
-        console.error('Failed to connect account');
-      }
-      setLoader(false);
-    } catch (error) {
-      setLoader(false);
-      console.error('Error:', error);
-      toast.error('Something Went Wrong');
-    }
   };
 
   const columns = [
@@ -96,69 +42,6 @@ const LinkedAccounts = () => {
 
   return (
     <>
-      {isLoading && (
-        <Box
-          sx={{
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            backgroundColor: 'rgba(255, 255, 255, 0.8)',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            zIndex: 9999
-          }}
-        >
-          <CircularProgress />
-        </Box>
-      )}
-      <Dialog open={modal} onClose={handleClose} fullWidth aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
-        {/* <DialogTitle id="alert-dialog-title">{'Enter Filter URL from LinkedIn'}</DialogTitle> */}
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <Grid container spacing={3}>
-                <Grid item xs={12}>
-                  <FormLabel>Enter Filter URL</FormLabel>
-                  <TextField
-                    {...register('url')}
-                    type="text"
-                    variant="outlined"
-                    fullWidth
-                    sx={{ marginTop: '8px' }}
-                    error={!!errors.url}
-                    helperText={errors.url?.message}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    fullWidth
-                    disabled={watch('url') ? false : true}
-                    sx={{
-                      height: '50px',
-                      borderRadius: '8px',
-                      backgroundColor: '#0A66C2',
-                      '&:hover': {
-                        backgroundColor: '#004182'
-                      }
-                    }}
-                    startIcon={<LinkedInIcon />}
-                  >
-                    Apply Filter
-                  </Button>
-                </Grid>
-              </Grid>
-            </form>
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Close</Button>
-        </DialogActions>
-      </Dialog>
       <ConnectAccount open={openDrawer} toggleDrawer={toggleDrawer} />
       <Container>
         <Stack direction="row" alignItems="center" justifyContent={'space-between'}>
@@ -169,9 +52,7 @@ const LinkedAccounts = () => {
                 Connect Account
               </Button>
             ) : (
-              <Button variant="contained" onClick={handleOpen}>
-                Generate Lead
-              </Button>
+              <Button variant="contained">Connected</Button>
             )}
           </Stack>
         </Stack>
